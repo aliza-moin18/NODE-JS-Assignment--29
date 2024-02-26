2a342376451bcc2a2918072e322093e85670789c
Aliza Moin <160245045+aliza-moin18@users.noreply.github.com>
2024-02-27 02:57:16 +0500

    Add files via upload

---
+++ Q29.js
@@ -0,0 +1,19 @@
+// 29. Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
+// • Make a array of your three favorite fruits and call it favorite_fruits.
+// • Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas!
+var favorite_fruits = ["Banana", "Mango", "Grapes"];
+if (favorite_fruits.includes("Mango")) {
+    console.log("You really like Mangos!");
+}
+if (favorite_fruits.includes("Strawberry")) {
+    console.log("You really like Strawberry!");
+}
+if (favorite_fruits.includes("Watemelon")) {
+    console.log("You really like Watermelon!");
+}
+if (favorite_fruits.includes("Bannana")) {
+    console.log("You really like Banana!");
+}
+// if(favorite_fruits.includes("Oranges")){
+//     console.log("You really like Oranges!")
+// }
